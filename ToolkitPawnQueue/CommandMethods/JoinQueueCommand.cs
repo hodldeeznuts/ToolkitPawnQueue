3b581f177cd84abeffcdd089a3871c4d16f0383d
hodlhodl <don@dmsgmail.com>
2020-03-27 01:05:30 -0700

    Merge Pawn Commands

---
+++ ToolkitPawnQueue/CommandMethods/JoinQueueCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using ToolkitCore;
+using ToolkitCore.Models;
+using TwitchLib.Client.Interfaces;
+using TwitchLib.Client.Models;
+using Verse;
+
+namespace ToolkitPawnQueue.Commands
+{
+    public class JoinQueueCommand : CommandMethod
+    {
+        public JoinQueueCommand(ToolkitChatCommand command) : base(command)
+        {
+        }
+
+        public override void Execute(ITwitchCommand twitchCommand)
+        {
+            GameComponentPawnTracking component = Current.Game.GetComponent<GameComponentPawnTracking>();
+
+            if (component.TryAddUsernameToQueue(twitchCommand.Username))
+            {
+                TwitchWrapper.SendChatMessage($"@{twitchCommand.Username} → You've joined the pawn queue.");
+            }
+            else
+            {
+                TwitchWrapper.SendChatMessage($"@{twitchCommand.Username} unable to join pawn queue.");
+            }
+        }
+    }
+}
